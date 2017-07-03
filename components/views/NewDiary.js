af1bf2ff2b438b0781e969b35f480f43783640ce
unknown <johannagr04@hotmail.com>
2017-07-03 00:24:02 -0600

    botones dentro de diario

--- components/views/NewDiary.js
+++ components/views/NewDiary.js
@@ -9,18 +9,28 @@ import {
 } from 'react-native';
 import React, {Component} from 'react';
 import { StackNavigator } from 'react-navigation';
-import { Container, Content, Form, Item, Input, Label, Button,Text,Body,CheckBox , Right, Switch, Icon, Card, CardItem, Thumbnail, Left,Image, Footer, FooterTab, Badge  } from 'native-base';
-import strings from '../common/local_strings.js'
-import FooterNav from  '../common/footerNav.js'
+import { Container, Content, Form, Item, Input, Label, Button ,Text,Body,CheckBox ,ActionSheet, Right, Switch, Icon, Card, CardItem, Thumbnail, Left,Image, Footer, FooterTab, Badge  } from 'native-base';
+import strings from '../common/local_strings.js';
+import FooterNav from  '../common/footerNav.js';
 import { getDatabase } from '../common/database';
-import  DailyList  from './dailyList';
-
+import  CreateDaily  from './createDaily.js';
+import PopupDialog, { SlideAnimation } from 'react-native-popup-dialog';
+import { DialogTitle } from 'react-native-popup-dialog';
+var BUTTONS = [
+  'Option 0',
+  'Option 1',
+  'Option 2',
+  'Delete',
+  'Cancel',
+];
+var DESTRUCTIVE_INDEX = 3;
+var CANCEL_INDEX = 4;
  export default class NewDiary extends Component {
   constructor(props){
     super(props)
     this.state = {
       name: '',      
-      status:'',
+      status: true,
       privacy: false,
       date:'',
       description: '',
@@ -31,18 +41,20 @@ import  DailyList  from './dailyList';
   privacyChange(){
     this.setState( {privacy: !this.state.privacy})
   }
+       // status:this.state.status,
+       // date:this.state.date,
   add(){
      getDatabase().ref().child('dairies/').push().set({
      diary: {
        name:this.state.name,
-       status:this.state.status,
        description:this.state.description,
        culture: this.state.culture,
        privacy:this.state.privacy,
-       date:this.state.date,
-
+       status:this.state.status,
      }
-   });
+   }).catch(function(error) {
+       alert(error);
+  });
 }
   // Nav options can be defined as a function of the screen's props:
   static navigationOptions = {
@@ -54,40 +66,54 @@ import  DailyList  from './dailyList';
         <Container>
           <Content>
             <Form>
-                <Right>
-                 <Label>{strings.privacy }</Label>
-                  <Switch
-                  value={ this.state.privacy }
-                    onValueChange={this.privacyChange.bind( this ) }/>
-                </Right>
+              <Right>
+                <Label>{strings.privacy }</Label>
+                <Switch 
+                value={ this.state.privacy }
+                onValueChange={this.privacyChange.bind( this ) }/>
+              </Right>
               <Item floatingLabel>
-                  <Label>{strings.name }</Label>
+                <Label>{strings.name }</Label>
                 <Input onChangeText={(text) => this.setState({name:text})}
                 returnKeyLabel = {"next"} />
               </Item>
               <Item floatingLabel>
-                  <Label>{strings.description }</Label>
+                <Label>{strings.description }</Label>
                 <Input onChangeText={(text) => this.setState({description:text})}
                 returnKeyLabel = {"next"}/>
               </Item>
               <Item floatingLabel>
-                  <Label>{strings.culture }</Label>
+                <Label>{strings.culture }</Label>
                 <Input onChangeText={(text) => this.setState({culture:text})}
                 returnKeyLabel = {"next"} />
               </Item>
-              <Item >
-            <Button iconRight  rounded info>
-              <Icon name='add' />
-              <Text>{strings.daily }</Text>
-            </Button>
-              </Item>
-              <Button block
-               onPress={() => this.add()}>
-               <Text>{strings.save }</Text>
-              </Button>
+               <Button rounded light>
+                  <Icon name='people' />
+                  <Text>{strings.guest }</Text>
+                </Button> 
+
+                <Button rounded light 
+                onPress={() => {this.popupDialog.show(); }}>
+                  <Icon name='calendar' />
+                  <Text>{strings.daily }</Text>
+                </Button>
             </Form>
-          </Content>
-        </Container>
+        </Content>
+            <Button full
+             onPress={() => this.add()}>
+             <Text>{strings.save }</Text>
+            </Button>   
+
+         <PopupDialog dialogTitle={<DialogTitle title={strings.daily } />}
+            ref={(popupDialog) => { this.popupDialog = popupDialog; }}
+            dialogAnimation = { new SlideAnimation({ slideFrom: 'bottom' }) }
+          >
+            <Container>
+             <CreateDaily></CreateDaily>
+            </Container>
+        </PopupDialog>
+      </Container>
+
 
     );
   }
